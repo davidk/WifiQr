d5cf52cd1a654aee9754b280a6c5d238b863b452
davidk <kdavid@gmail.com>
2020-03-28 17:58:40 -0700

    Bump version to 0.0.6

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use clap::{App, Arg, ArgGroup};
 
 fn main() {
     let options = App::new("WifiQR")
-        .version("0.0.5")
+        .version("0.0.6")
         .about("Encode your wi-fi credentials as a scannable QR code")
         .author("davidk")
         .usage("wifiqr [ --ssid (ssid) ] [ --password (password) | --ask ]

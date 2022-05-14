2f19d6c9fce85ae7277855a206f7541ccbf86634
davidk <kdavid@gmail.com>
2022-05-14 16:35:44 -0700

    lib.rs: Add svg string exporter from  module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 /// wifiqr
 /// A crate to transform Wifi credentials into a scannable QR code
-
 extern crate image;
 extern crate qrcodegen;
+
+mod exporters;
 
 macro_rules! wifi_auth {
     // Derived from:
@@ -12,9 +13,15 @@ macro_rules! wifi_auth {
     // S: network SSID
     // P: wifi password. Can be ommitted if T is 'nopass'
     // H: Hidden SSID. Optional.
-    (hidden) => ("WIFI:T:{};S:{};P:{};H:{};;");
-    (nopass) => ("WIFI:T:nopass;S:{};;");
-    (nopass_hidden) => ("WIFI:T:nopass;S:{};H:{};;");
+    (hidden) => {
+        "WIFI:T:{};S:{};P:{};H:{};;"
+    };
+    (nopass) => {
+        "WIFI:T:nopass;S:{};;"
+    };
+    (nopass_hidden) => {
+        "WIFI:T:nopass;S:{};H:{};;"
+    };
     () => {
         "WIFI:T:{};S:{};P:{};;"
     };
@@ -30,7 +37,9 @@ mod tests {
     #[test]
     fn test_credentials() {
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("WPA2"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("WPA2"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:WPA2;S:test;P:password;;"
         );
     }
@@ -39,10 +48,15 @@ mod tests {
     #[test]
     fn test_credentials_escapes() {
         assert_eq!(
-            Credentials::new(Some(r###""foo;bar\baz""###), 
-                             Some("randompassword"), 
-                             Some("wpa2"), 
-                             false, false).format().unwrap(),
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some("randompassword"),
+                Some("wpa2"),
+                false,
+                false
+            )
+            .format()
+            .unwrap(),
             r###"WIFI:T:WPA2;S:\"foo\;bar\\baz\";P:randompassword;;"###
         );
     }
@@ -64,57 +78,95 @@ mod tests {
         );
     }
 
-    /// Ensure that the hidden flag is added if requested 
+    /// Ensure that the hidden flag is added if requested
     #[test]
     fn test_hidden_ssid() {
-        assert_eq!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some("randompassword"), 
-                                    Some("WPA2"), true, false).format().unwrap(),
-            r###"WIFI:T:WPA2;S:\"foo\;bar\\baz\";P:randompassword;H:true;;"###);
+        assert_eq!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some("randompassword"),
+                Some("WPA2"),
+                true,
+                false
+            )
+            .format()
+            .unwrap(),
+            r###"WIFI:T:WPA2;S:\"foo\;bar\\baz\";P:randompassword;H:true;;"###
+        );
     }
 
     /// If a ssid isn't hidden, it shouldn't be set in the formatted string
     #[test]
     fn test_normal_ssid() {
-        assert_eq!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some("randompassword"), 
-                                    Some("WPA2"), false, false).format().unwrap(),
-            r###"WIFI:T:WPA2;S:\"foo\;bar\\baz\";P:randompassword;;"###);
+        assert_eq!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some("randompassword"),
+                Some("WPA2"),
+                false,
+                false
+            )
+            .format()
+            .unwrap(),
+            r###"WIFI:T:WPA2;S:\"foo\;bar\\baz\";P:randompassword;;"###
+        );
     }
 
     /// Require a password when wpa/wpa2 is requested
     #[test]
     fn test_nopassword_with_wpa2() {
-        assert!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some(""), 
-                                    Some("wpa"), 
-                                    false, false).format().is_err(), 
-        "wpa2 requires a password");
- 
-        assert!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some(""), 
-                                    Some("wpa2"), 
-                                    false, false).format().is_err(), 
-        "wpa2 requires a password");
+        assert!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some(""),
+                Some("wpa"),
+                false,
+                false
+            )
+            .format()
+            .is_err(),
+            "wpa2 requires a password"
+        );
+
+        assert!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some(""),
+                Some("wpa2"),
+                false,
+                false
+            )
+            .format()
+            .is_err(),
+            "wpa2 requires a password"
+        );
     }
 
     /// Require a password when using wep
     #[test]
     fn test_nopassword_with_wep() {
-        assert!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some(""), 
-                                    Some("wep"), 
-                                    false, false).format().is_err(), 
-        "wep requires a password");
+        assert!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some(""),
+                Some("wep"),
+                false,
+                false
+            )
+            .format()
+            .is_err(),
+            "wep requires a password"
+        );
     }
 
     #[test]
     fn test_nopassword_with_nopassword() {
-        assert!(Credentials::new(Some("bane"), 
-                                    Some(""), 
-                                    Some("nopass"), 
-                                    false, false).format().is_ok(), 
-        "nopass specified with a blank password should work");
+        assert!(
+            Credentials::new(Some("bane"), Some(""), Some("nopass"), false, false)
+                .format()
+                .is_ok(),
+            "nopass specified with a blank password should work"
+        );
     }
 
     /// Test various auth (T) types, like WPA/WPA2
@@ -122,50 +174,69 @@ mod tests {
     fn test_auth_types() {
         // wep
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("wep"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("wep"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:WEP;S:test;P:password;;"
         );
 
         // wpa
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("WPA"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("WPA"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:WPA;S:test;P:password;;"
         );
 
         // wpa2 -- note that the wifi string has WPA2 in caps. it seems that iOS devices are sensitive
         // to the T: parameter being lowercase (and will return 'no usable data found')
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("wpa2"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("wpa2"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:WPA2;S:test;P:password;;"
         );
 
         // wpa3
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("wpa3"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("wpa3"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:WPA3;S:test;P:password;;"
         );
 
         // nopass -- unlike wpa2/wpa3, etc, nopass is accepted by iOS devices uncapitalized
         assert_eq!(
-            Credentials::new(Some("test"), Some(""), Some("nopass"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some(""), Some("nopass"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:nopass;S:test;;"
         );
     }
 
     #[test]
     fn test_empty_passwords_with_nopass_encr() {
-        assert!(Credentials::new(Some(r###""foo;bar\baz""###), 
-                                    Some("password"), 
-                                    Some("nopass"), 
-                                    false, false).format().is_err(), 
-        "nopass cannot be specified with a password");
+        assert!(
+            Credentials::new(
+                Some(r###""foo;bar\baz""###),
+                Some("password"),
+                Some("nopass"),
+                false,
+                false
+            )
+            .format()
+            .is_err(),
+            "nopass cannot be specified with a password"
+        );
     }
 
     /// ensure that nopass is set along with an empty password when it is requested by the user
     #[test]
     fn test_encr_nopass_with_empty_password() {
         assert_eq!(
-            Credentials::new(Some("test"), Some(""), Some("nopass"), false, false).format().unwrap(),
+            Credentials::new(Some("test"), Some(""), Some("nopass"), false, false)
+                .format()
+                .unwrap(),
             "WIFI:T:nopass;S:test;;"
         );
     }
@@ -174,7 +245,9 @@ mod tests {
     #[test]
     fn test_quoted_ssid_password() {
         assert_eq!(
-            Credentials::new(Some("test"), Some("password"), Some("wpa2"), false, true).format().unwrap(),
+            Credentials::new(Some("test"), Some("password"), Some("wpa2"), false, true)
+                .format()
+                .unwrap(),
             "WIFI:T:WPA2;S:\"test\";P:\"password\";;"
         );
     }
@@ -190,6 +263,8 @@ pub mod code {
 
     use imageproc::drawing::draw_filled_rect_mut;
     use imageproc::rect::Rect;
+
+    use crate::exporters::svg::to_svg_string;
 
     #[derive(Debug)]
     pub struct Credentials {
@@ -222,9 +297,10 @@ pub mod code {
         /// Special characters `\`, `;`, `,` and `:` should be escaped with a backslash
         fn filter_credentials(&self, field: &str) -> String {
             // N.B. If performance problems ever crop up, this might be more performant
-            // with regex replace_all 
+            // with regex replace_all
 
-            let mut filtered = field.to_string()
+            let mut filtered = field
+                .to_string()
                 .replace(r#"\"#, r#"\\"#)
                 .replace(r#"""#, r#"\""#)
                 .replace(r#";"#, r#"\;"#)
@@ -235,7 +311,7 @@ pub mod code {
                 filtered = format!("\"{}\"", field.to_string());
             }
 
-            return filtered
+            return filtered;
         }
 
         /// the encryption field in the Wifi QR code fails on iOS devices if it is
@@ -248,7 +324,7 @@ pub mod code {
             return field.to_string();
         }
 
-        /// Call the wifi_auth! macro to generate a qr-string and/or return any errors that 
+        /// Call the wifi_auth! macro to generate a qr-string and/or return any errors that
         /// need to be raised to the caller. Note: format does not enforce an encryption type, it is
         /// up to the end user to use the right value if one is provided.
         pub fn format(&self) -> Result<String, &'static str> {
@@ -263,9 +339,11 @@ pub mod code {
 
             if self.encr == "nopass" || self.encr.is_empty() {
                 if !self.pass.is_empty() {
-                    return Err("With nopass as the encryption type (or unset encryption type), 
+                    return Err(
+                        "With nopass as the encryption type (or unset encryption type), 
                         the password field should be empty. (Encryption should probably be set 
-                        to something like wpa2)")
+                        to something like wpa2)",
+                    );
                 }
             }
 
@@ -273,7 +351,7 @@ pub mod code {
                 // Error condition: Password is empty, and the T (encr) type is not "nopass" / not empty
                 if self.encr != "nopass" && !self.encr.is_empty() {
                     return Err("The encryption method requested requires a password. 
-                        If you would like no password, set '--encr nopass'")
+                        If you would like no password, set '--encr nopass'");
                 }
 
                 if self.encr.is_empty() || self.encr == "nopass" {
@@ -299,31 +377,31 @@ pub mod code {
                     self.filter_credentials(&self.ssid),
                     self.filter_credentials(&self.pass),
                     &self.hidden,
-                ))
+                ));
             } else {
                 return Ok(format!(
                     wifi_auth!(),
                     self.filter_credentials(&self.filter_encr(&self.encr)),
                     self.filter_credentials(&self.ssid),
                     self.filter_credentials(&self.pass)
-                ))
+                ));
             }
-        }
-
-        // Transform the QR Wifi connection string into a Vec<char> for use with manual_encode()
-        pub fn format_vec(&self) -> Vec<char> {
-            return Credentials::format(&self).unwrap().chars().collect();
         }
     }
 
     /// returns a new Credentials struct given Wifi credentials. This data is not validated,
     /// nor formatted into a QR code string. Call .format() on Credentials to do this.
-    pub fn auth(_ssid: Option<&str>, _password: Option<&str>, _encr: Option<&str>, 
-        _hidden: bool, _quote: bool) -> Credentials {
+    pub fn auth(
+        _ssid: Option<&str>,
+        _password: Option<&str>,
+        _encr: Option<&str>,
+        _hidden: bool,
+        _quote: bool,
+    ) -> Credentials {
         return self::Credentials::new(_ssid, _password, _encr, _hidden, _quote);
     }
 
-    /// generates a qrcode from a Credentials configuration 
+    /// generates a qrcode from a Credentials configuration
     pub fn encode(config: &Credentials) -> Result<QrCode, DataTooLong> {
         let q = QrCode::encode_text(&config.format().unwrap(), QrCodeEcc::High)?;
         Ok(q)
@@ -332,11 +410,16 @@ pub mod code {
     /// manual_encode isn't intended for use externally, but exists to compare between the
     /// automated encoder and this manual_encode version
     /// https://docs.rs/qrcodegen/latest/src/qrcodegen/lib.rs.html#151
-    pub fn manual_encode(config: &Credentials, error_level: QrCodeEcc, lowest_version: qrcodegen::Version, 
-        highest_version: qrcodegen::Version, mask_level: Option<Mask>) -> QrCode {
+    pub fn manual_encode(
+        config: &Credentials,
+        error_level: QrCodeEcc,
+        lowest_version: qrcodegen::Version,
+        highest_version: qrcodegen::Version,
+        mask_level: Option<Mask>,
+    ) -> QrCode {
+        //             return &Credentials::format(&self).unwrap();
 
-        let wifi: Vec<char> = config.format_vec();
-        let segs: Vec<QrSegment> = QrSegment::make_segments(&wifi);
+        let segs: Vec<QrSegment> = QrSegment::make_segments(&config.format().unwrap());
 
         return QrCode::encode_segments_advanced(
             &segs,
@@ -345,15 +428,17 @@ pub mod code {
             highest_version,
             mask_level,
             true,
-        ).unwrap();
+        )
+        .unwrap();
     }
 
     /// generates an svg string from a QrCode (output from the QR library)
     ///
     /// * qrcode: &QrCode
     ///
+
     pub fn make_svg(qrcode: &QrCode) -> String {
-        return qrcode.to_svg_string(4);
+        return to_svg_string(&qrcode, 4);
     }
 
     /// generates a wifi qr code that is printed to a terminal/console for quick scanning
@@ -407,14 +492,18 @@ pub mod code {
 
     /// returns an ImageBuffer<> that can be saved using save_image(), or passed on
     /// for further manipulation by the caller
-    /// 
+    ///
     ///
     /// * qrcode: Is an encoded qrcode
-    /// 
+    ///
     /// * scale: The scaling factor to apply to the qrcode
-    /// 
+    ///
     /// * border_size: How large to make the quiet zone
-    pub fn make_image(qrcode: &QrCode, scale: i32, border_size: i32) -> ImageBuffer<LumaA<u8>, Vec<u8>> {
+    pub fn make_image(
+        qrcode: &QrCode,
+        scale: i32,
+        border_size: i32,
+    ) -> ImageBuffer<LumaA<u8>, Vec<u8>> {
         let new_qr_size = qrcode.size() * scale;
 
         // --- Initialize to a white canvas with the alpha layer pre-set ---
@@ -433,7 +522,8 @@ pub mod code {
                         Rect::at(
                             (x * scale) + border_size as i32,
                             (y * scale) + border_size as i32,
-                        ).of_size(scale as u32, scale as u32),
+                        )
+                        .of_size(scale as u32, scale as u32),
                         LumaA([0, 255]),
                     );
                 } else {
@@ -442,7 +532,8 @@ pub mod code {
                         Rect::at(
                             (x * scale) + border_size as i32,
                             (y * scale) + border_size as i32,
-                        ).of_size(scale as u32, scale as u32),
+                        )
+                        .of_size(scale as u32, scale as u32),
                         LumaA([255, 255]),
                     );
                 }
@@ -451,7 +542,6 @@ pub mod code {
 
         return image;
     }
-
 
     /// saves an image to a file
     ///
